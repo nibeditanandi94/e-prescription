962bb9b8a5fff0637164c34b29ac89ae09f7c217
Nibedita Nandi <nibeditanandi1@gmail.com>
2021-06-12 21:31:37 -0500

    appInitializer for logout fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+//import { UserService } from './core/services/user.service';
+
+/*export function login(userService: UserService) {
+ return() => userService.login;
+}*/
+
+export function logOut(auth: AngularFireAuth) {
+  return() => auth.signOut;
+ }
 
 @NgModule({
   declarations: [
@@ -22,7 +32,14 @@ import {AngularFirestoreModule} from '@angular/fire/firestore';
     appId: "1:581072553879:web:31c653ab030acf49ae26d2"}),
     AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory : logOut,
+      deps: [AngularFireAuth],
+      multi : true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/patient/patient-list/patient-list.component.ts
+++ src/app/patient/patient-list/patient-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IPatientData } from '../../patientInterface';
 
 @Component({
   selector: 'app-patient-list',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-list.component.css']
 })
 export class PatientListComponent implements OnInit {
+  patientDataCollection : AngularFirestoreCollection<any>;
+  pItem : Observable<IPatientData[]>;
+ 
 
-  constructor() { }
-
+  constructor(private firestore : AngularFirestore) { 
+   
+  
+  }
+ 
   ngOnInit(): void {
+    this.patientDataCollection = this.firestore.collection('/patientData');
+    this.pItem = this.patientDataCollection.snapshotChanges().pipe( 
+     map(actions => actions.map(a => {
+       const data = a.payload.doc.data() as IPatientData;
+       const id = a.payload.doc.id;
+       return { id, ...data };
+     }))
+   );
+
+    this.getPatientData();
+
   }
 
+  getPatientData(){
+    this.pItem.subscribe(item => {
+    console.log(item);
+   })
+    
+  }
 }
